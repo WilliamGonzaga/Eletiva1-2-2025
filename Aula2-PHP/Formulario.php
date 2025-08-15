faf66206d462634e8f6f33e585709ab8e99a7775
WilliamGonzaga <williamgonzagadasantacruz@gmail.com>
2025-08-14 22:04:21 -0300

    desafio soma em php feito

--- Aula2-PHP/Formulario.php
+++ Aula2-PHP/Formulario.php
@@ -7,7 +7,8 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
-    <div class="container">
+    
+    <!-- <div class="container">
         <h1>Primeiro exemplo de formulario</h1>
         <form method="post" action="FormularioResp.php">
         <div class="mb-3">
@@ -15,8 +16,23 @@
                     <input type="text" id="nome" name="nome" class="form-control" required="">
                     </div>
         <button type="submit" class="btn btn-primary">Enviar</button>
-    </form>
+        </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div> -->
+    
+
+    <div class="container">
+        <h1>Primeiro exemplo de formulario</h1>
+        <form method="post" action="FormularioResp.php">
+        <div class="row inline-row mb-3"><div class="col-md-6">
+                    <label for="numero1" class="form-label">numero1</label>
+                    <input type="number" id="numero1" name="numero1" class="form-control" required="">
+                    </div><div class="col-md-6">
+                    <label for="numero2" class="form-label">numero2</label>
+                    <input type="number" id="numero2" name="numero2" class="form-control" required="">
+                    </div></div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
     </div>
 </body>
 </html>
--- Aula2-PHP/FormularioResp.php
+++ Aula2-PHP/FormularioResp.php
@@ -14,9 +14,16 @@
 
 <?php
 
-    $nome = $_POST['nome'];
-    echo "Seja bem vindo(a) $nome";
+   //nome = $_POST['nome'];
+  //echo "Seja bem vindo(a) $nome";
 
+
+    $numero1 = $_POST['numero1'];
+    $numero2 = $_POST['numero2'];
+
+    $soma = $numero1 + $numero2;
+
+    echo "soma dos numero informado é $soma";
 
     ?>
 
